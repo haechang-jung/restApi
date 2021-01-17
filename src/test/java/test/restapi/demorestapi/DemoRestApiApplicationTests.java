25a768d4d9932b7e45b166bb4b5adffd123a7948
haechang.jung <smilehc88@gmail.com>
2021-01-17 23:23:04 +0900

    project creater

---
+++ src/test/java/test/restapi/demorestapi/DemoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package test.restapi.demorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
