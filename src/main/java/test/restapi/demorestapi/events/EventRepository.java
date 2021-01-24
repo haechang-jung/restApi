7c951c5cba4382a7dfead4a27624f2447281f7e0
haechang.jung <smilehc88@gmail.com>
2021-01-24 15:26:40 +0900

    eventController

---
+++ src/main/java/test/restapi/demorestapi/events/EventRepository.java
@@ -0,0 +1,7 @@
+package test.restapi.demorestapi.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
